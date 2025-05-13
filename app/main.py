d1520539cf8db9d2c3cb706a2901ca3e7622604f
hnhiyeudau <86763744+hnhiyeudau@users.noreply.github.com>
2025-05-13 14:34:42 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,14 +4,19 @@ from app.service.predict import predict_image
 
 app = FastAPI()
 
-origins = [
-    "https://pcmmd-frontend-mv2w5lz5s-nhutduys-projects.vercel.app",
-    "http://localhost:3000",  # Cho phép dùng local test luôn
-]
+# origins = [
+#     "https://pcmmd-frontend-mv2w5lz5s-nhutduys-projects.vercel.app",
+#     "http://localhost:3000",  # Cho phép dùng local test luôn
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+    allow_origins=["https://pcmmd-frontend-mv2w5lz5s-nhutduys-projects.vercel.app",
+    "http://localhost:3000",
+    ], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"]
 )
 
 @app.get("/")
